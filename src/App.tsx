cd327d2bc9678f710b6e629fb01667d716d7898c
brudolce <brudolce@gmail.com>
2020-12-27 14:06:09 -0300

    store on app

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Game from "./views/Game/Game";
 import Home from "./views/Home/Home";
 import GameOver from "./views/GameOver/GameOver";
+import store from './store';
+import { Provider } from 'react-redux';
 
 const App: React.FC = () => {
   return (
-    <>
+    <> <Provider store={store}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
@@ -14,6 +16,7 @@ const App: React.FC = () => {
           <Route exact path="/gameover" component={GameOver} />
         </Switch>
       </BrowserRouter>
+      </Provider>
     </>
   );
 };
