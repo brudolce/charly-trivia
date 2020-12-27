c1362496e848bf5ec8a703973c50a4a3970a656e
brudolce <brudolce@gmail.com>
2020-12-27 14:10:38 -0300

    https on fetching

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Game from "./views/Game/Game";
 import Home from "./views/Home/Home";
 import GameOver from "./views/GameOver/GameOver";
-import store from './store';
-import { Provider } from 'react-redux';
+import store from "./store";
+import { Provider } from "react-redux";
 
 const App: React.FC = () => {
   return (
-    <> <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/game" component={Game} />
-          <Route exact path="/gameover" component={GameOver} />
-        </Switch>
-      </BrowserRouter>
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/game" component={Game} />
+            <Route exact path="/gameover" component={GameOver} />
+          </Switch>
+        </BrowserRouter>
       </Provider>
     </>
   );
--- src/views/Game/fetchData.ts
+++ src/views/Game/fetchData.ts
@@ -2,7 +2,7 @@ import { QuestionObject } from "../../store/ducks/question/types";
 
 export const fetchQuestion = async (): Promise<QuestionObject|any> => {
   try {
-    const res = await fetch("http://jservice.io/api/random");
+    const res = await fetch("https://jservice.io/api/random");
     const data = await res.json();
     return data[0];
   } catch (e) {
